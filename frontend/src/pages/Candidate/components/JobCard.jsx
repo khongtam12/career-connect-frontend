de81e7636c5fe690f6c634614f1b6fb0d4e67dd1
Minhhieu1001 <hieuminhpham1001@gmail.com>
2026-04-23 23:14:17 +0700

    job-detail

--- frontend/src/pages/Candidate/components/JobCard.jsx
+++ frontend/src/pages/Candidate/components/JobCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Heart } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import {getJobById} from "../../../service/jobService";
+
 
 
 export default function JobCard({ job, isFeatured = false, onDetail }) {
