471a910faaf95e8da74c262477d8e1609a9d6517
Khổng Tám <22690521.tam@student.iuh.edu.vn>
2026-06-01 23:52:47 +0700

    fix ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
-
+//
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
-
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={googleClientId}>
     <App />
